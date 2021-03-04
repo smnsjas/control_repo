7ee26cbcef56f7b2aab74b17860f5b5e7d28d00b
Jason Simons <11409991+smnsjas@users.noreply.github.com>
2021-03-04 10:10:03 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,4 @@
 node default {
-  file {'/root/README':
-    ensure  => file, 
-    content => 'This is my readme',
-    owner   => 'root',
   }
   node 'master.puppet.vm' {
     include role::master_server
