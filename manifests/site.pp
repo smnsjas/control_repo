8138c4a00ab81a85a10f920bcc985d8e38b27a30
Jason Simons <11409991+smnsjas@users.noreply.github.com>
2021-03-04 17:45:39 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,8 +2,14 @@ node default {
 }
 node 'master.puppet.vm' {
    include role::master_server
+   file {'/root/README':
+      ensure => file,
+      content => $fqdn,
+   }
 }
 node /^web/ {
    include role::app_server
-   include role::db_server
+}
+node /^db {
+    include role::db_server
 }
