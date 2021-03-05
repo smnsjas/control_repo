03445284b2f498c61b5709b4edc0c0043e4bbda8
Jason Simons <11409991+smnsjas@users.noreply.github.com>
2021-03-05 16:24:07 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,14 @@
 node default {
 }
-node 'master.puppet.vm' {
-   include role::minecraft_server  
+node 'master.puppet.vm' { 
    include role::master_server
    file {'/root/README':
       ensure => file,
       content => "Welcome to ${fqdn}\n", 
    }
+}
+node 'minetest.puppet.vm' {
+   include role::minecraft_server 
 }
 node /^web/ {
    include role::app_server
