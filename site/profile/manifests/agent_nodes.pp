4f86108266921f3d28ee8878c2995bece11a7fdf
Jason Simons <11409991+smnsjas@users.noreply.github.com>
2021-03-04 19:27:29 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -7,7 +7,7 @@ class profile::agent_nodes {
     ip     => '172.18.0.3',
   }
    host {'db.puppet.vm':
-    ensure => present,
-    ip     => '172.18.0.2',
+     ensure => present,
+     ip     => '172.18.0.2',
   }
 }
