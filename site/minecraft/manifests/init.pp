0893a8579daa4b380ecf1db6ecaeb7eaa3c79c07
Jason Simons <11409991+smnsjas@users.noreply.github.com>
2021-03-05 16:47:56 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -12,7 +12,7 @@ class minecraft {
     ensure => file,
     content => 'eula=true'
   }
-  file {'etc/systemd/system/minecraft.service':
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service'
   }
