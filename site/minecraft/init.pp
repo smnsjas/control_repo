eb56dbdc64376d33ae07e8c2e472477675786695
Jason Simons <11409991+smnsjas@users.noreply.github.com>
2021-03-05 14:50:24 -0500

    Update init.pp

--- site/minecraft/init.pp
+++ site/minecraft/init.pp
@@ -15,5 +15,9 @@ class minecraft {
   file {'etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service'
-  } 
+  }
+  service {'minecraft':
+    ensure => running,
+    enabled => true,
+  }
 }
