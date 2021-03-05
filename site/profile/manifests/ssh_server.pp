c663b2aa67ca71e2fb7c400c28ba7b7ef2a8b050
Jason Simons <11409991+smnsjas@users.noreply.github.com>
2021-03-04 19:09:49 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABgQC2RfBxgB9Z5N3sCEX1GK+EnhVvkA/teascYbvOp8p/G2l8cKdY1Qzynmd33FJxl2KFJ4tBpr79ZEN4IthHu/0W/h3GO8kFnORZTgWZ7mPjI/07opcKVZ4scbPJ/i/kv6n+9vDp3FlAF3KxVp/6ice+Zs9FVBpMSE6EC5BXoVUO+BtotpeujM93aGhSZlzlBCY3wmTA0/2BRypgEXoZnrspc6Y0tbgB5oJuayjcKyjLmEsqXUUJLLHEFNwF8FIB0r8Nb92DcTclwHsUtVeswCbqbae3L3wfOW51+cKUBno3WQ8lejLi+enUO59XytWqp+xoODLuDnhxtEQOqr8qOfxzawQuh+1LJMF/qvPxHaFCYPFVDICT5IQopBjJo6TUSoZ2yC2+Mxkmy2z3L+8HTtwmqvi05Zn0KBYHWVwjXoQusLiz1qzkRqNQrlH1P+VlfwEtUI8SkLlfd2sBFpoR4kwV2Qk253TC9KV3zV/rYT3EzC/lDsaA9Idfy6PkKFwuTMk=',
   }  
 }
